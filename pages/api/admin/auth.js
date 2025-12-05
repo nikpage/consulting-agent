2972482141c1fcb970bcec545a75398a863327fd
Nik Page <nbpage@gmail.com>
2025-12-05 20:09:46 +0100

    a

--- pages/api/admin/auth.js
+++ pages/api/admin/auth.js
@@ -1,6 +1,8 @@
 export default function handler(req, res) {
   if (req.method === 'POST') {
     const { password } = req.body
+    console.log('Received:', password)
+    console.log('Expected:', process.env.ADMIN_PASSWORD)
     if (password === process.env.ADMIN_PASSWORD) {
       res.status(200).json({ success: true })
     } else {
