3e3766a06e3174c35711c78b5a12cfecebf23496
Nik Page <nbpage@gmail.com>
2026-01-09 14:55:25 +0100

    Update index.ts

--- pages/api/auth/google/index.ts
+++ pages/api/auth/google/index.ts
@@ -2,6 +2,9 @@ import { getAuthUrl } from '../../../../lib/google-auth';
 
 export default function handler(req: any, res: any) {
   const userId = req.query.user_id;
-  const url = getAuthUrl(userId);
+
+  const baseUrl = getAuthUrl();
+  const url = baseUrl + `&state=${encodeURIComponent(userId)}`;
+
   res.redirect(url);
 }
